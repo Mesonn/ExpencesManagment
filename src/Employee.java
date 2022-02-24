05c902108dc03ed826152f70162beedafa293e5b
dalli <dali.goundi@gmail.com>
2022-02-24 04:11:39 +0100

    some Tests

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,60 @@
 public class Employee {
+    private int id;
+    private String title ;
+    private String firstName;
+    private String surName;
+    private String jobTitle;
+    private String department;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setFirstName(String firstName) {
+        if (firstName.length() < 2){
+            System.out.println("Error! Firstname must be at least 2 characters");
+        }else {
+        this.firstName = firstName;
+        }
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee();
+        employee1.setId(1);
+        employee1.setFirstName("Dali");
+        employee1.setSurName("Goundi");
+
+    }
+}
